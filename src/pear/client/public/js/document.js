5d0f67f6766f82236f144ad5847f5e5cc7297084
Charles Chong <CC.Charles.Chong@gmail.com>
2014-04-20 18:14:48 -0400

    changed modal to resize the text editor

--- src/pear/client/public/js/document.js
+++ src/pear/client/public/js/document.js
@@ -1,8 +1,31 @@
-$(function(){
-    $("#openSidePanelButton").pageslide({ direction: "left", modal: true });
-
+$(function(){   
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/cobalt");
     editor.getSession().setMode("ace/mode/javascript");
     editor.getSession().setUseWrapMode(true);
+
+    $("#openSidePanelButton").click(function(){
+        $("#editor").animate({right:"300px"}, {
+            duration: 500,
+            start: function(){
+                $.pageslide({ direction: "left", speed:500, modal: true , href: "#sidePanel"});
+            },
+            progress: function(){
+                editor.resize();
+                console.log("RAWR")
+            }
+        });
+    });
+    $("#closeSidePanelButton").click(function(){
+        $("#editor").animate({right:"0px"}, {
+            duration: 500,
+            start: function(){
+                $.pageslide.close();
+            },
+            progress: function(){
+                editor.resize();
+                console.log("RAWR")
+            }
+        });
+    });
 });
